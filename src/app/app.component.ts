8a3e6de98132b29b55c3db25ef07616b1e198cab
Tümmers, David <david.tuemmers@connext.de>
2024-09-09 10:57:29 +0200

    initital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MoneyManagerPwa';
+  savedValue: string | null = null;  // Variable to hold the saved value
+
+  ngOnInit(): void {
+    // Check if the value exists in localStorage
+    const valueFromStorage = localStorage.getItem('savedValue');
+
+    if (valueFromStorage) {
+      // If the value exists, assign it to the variable
+      this.savedValue = valueFromStorage;
+    } else {
+      // If the value doesn't exist, set a default value and save it to localStorage
+      localStorage.setItem('savedValue', this.savedValue ?? 'lol');
+    }
+  }
+
+  onSaveValueChanged() {
+    localStorage.setItem('savedValue', this.savedValue ?? 'lol');
+  }
+
 }
