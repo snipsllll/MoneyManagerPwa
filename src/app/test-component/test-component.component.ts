3f67f0654d67ada552df36571e2416fdb184ef97
Tümmers, David <david.tuemmers@connext.de>
2024-09-09 16:43:31 +0200

    environment

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-test-component',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './test-component.component.css'
 })
 export class TestComponentComponent {
+  isDevelopment!: boolean;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // Use the environment variable to set the boolean
+    this.isDevelopment = !environment.production;
+  }
 }
