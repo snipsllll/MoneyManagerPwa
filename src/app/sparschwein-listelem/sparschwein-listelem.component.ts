f3a7e084c0f6e4c3cc615c1fd5288fbe3e7444ba
Tümmers, David <david.tuemmers@connext.de>
2024-09-10 14:56:20 +0200

    sparschwein-listelem component

---
+++ src/app/sparschwein-listelem/sparschwein-listelem.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sparschwein-listelem',
+  templateUrl: './sparschwein-listelem.component.html',
+  styleUrl: './sparschwein-listelem.component.css'
+})
+export class SparschweinListelemComponent {
+
+}
