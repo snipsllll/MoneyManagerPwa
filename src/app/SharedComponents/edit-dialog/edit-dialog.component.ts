c3606d9f559545a5c924b107d930350024322af3
Tümmers, David <david.tuemmers@connext.de>
2024-09-12 11:26:51 +0200

    edit dialog finished

--- src/app/SharedComponents/edit-dialog/edit-dialog.component.ts
+++ src/app/SharedComponents/edit-dialog/edit-dialog.component.ts
@@ -1,19 +1,30 @@
-import {Component, Input, signal} from '@angular/core';
-import {EditDialogViewModel} from "../../Models/ViewModels/EditDialogViewModel";
+import {Component, Input, OnInit, signal} from '@angular/core';
+import {EditDialogData, EditDialogViewModel} from "../../Models/ViewModels/EditDialogViewModel";
 
 @Component({
   selector: 'app-edit-dialog',
   templateUrl: './edit-dialog.component.html',
   styleUrl: './edit-dialog.component.css'
 })
-export class EditDialogComponent {
+export class EditDialogComponent implements OnInit{
 
   @Input() viewModel!: EditDialogViewModel;
   showBetragWarnung = signal<boolean>(false);
+  darfSpeichern = signal<boolean>(false);
+  oldEintrag!: EditDialogData;
 
+  ngOnInit() {
+    this.oldEintrag = this.viewModel.data
+  }
 
   onSaveClicked() {
-    this.viewModel.data.onSaveClick;
+    if(!this.checkBetragValid()) {
+      this.showBetragWarnung.set(true);
+      return;
+    }
+    if(this.checkDarfSpeichern()) {
+      this.viewModel.data.onSaveClick;
+    }
   }
 
   onCancelClicked() {
@@ -22,5 +33,17 @@ export class EditDialogComponent {
 
   onBackgroundClicked() {
     console.log(68736742634632872)
+  }
+
+  onValueChanged() {
+    this.darfSpeichern.set(this.checkDarfSpeichern());
+  }
+
+  checkDarfSpeichern() {
+    return (this.oldEintrag.title !== this.viewModel.data.title || this.oldEintrag.betrag !== this.viewModel.data.betrag || this.oldEintrag.zusatz !== this.viewModel.data.zusatz) && this.checkBetragValid()
+  }
+
+  checkBetragValid() {
+    return (this.viewModel.data.betrag !== 0 && this.viewModel.data.betrag !== null);
   }
 }
