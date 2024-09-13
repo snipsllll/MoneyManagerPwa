72612455165876de56c95ea40569ec341a87f09f
Tümmers, David <david.tuemmers@connext.de>
2024-09-13 11:35:29 +0200

    basic structure

--- src/app/Sites/Wunschliste/wunschliste/wunschliste.component.ts
+++ src/app/Sites/Wunschliste/wunschliste/wunschliste.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TopbarService} from "../../../Services/TopBarService/topbar.service";
 
 @Component({
   selector: 'app-wunschliste',
   templateUrl: './wunschliste.component.html',
   styleUrl: './wunschliste.component.css'
 })
-export class WunschlisteComponent {
+export class WunschlisteComponent implements OnInit{
 
+  constructor(private topbarService: TopbarService) {
+  }
+
+  ngOnInit() {
+    this.topbarService.title.set('WUNSCHLISTE');
+    this.topbarService.dropDownSlidIn.set(false);
+    this.topbarService.isDropDownDisabled = true;
+  }
+
+  onPlusClicked() {
+
+  }
 }
