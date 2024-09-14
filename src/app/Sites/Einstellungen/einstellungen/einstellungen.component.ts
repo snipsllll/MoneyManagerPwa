428fa101f96918d28ae0d529f7140ed1f894a217
Tümmers, David <david.tuemmers@connext.de>
2024-09-14 16:46:03 +0200

    einstellungen basic structure (ui)

--- src/app/Sites/Einstellungen/einstellungen/einstellungen.component.ts
+++ src/app/Sites/Einstellungen/einstellungen/einstellungen.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TopbarService} from "../../../Services/TopBarService/topbar.service";
 
 @Component({
   selector: 'app-einstellungen',
   templateUrl: './einstellungen.component.html',
   styleUrl: './einstellungen.component.css'
 })
-export class EinstellungenComponent {
+export class EinstellungenComponent implements OnInit{
+
+  constructor(private topbarService: TopbarService) {
+  }
+
+  ngOnInit() {
+    this.topbarService.title.set('EINSTELLUNGEN');
+    this.topbarService.dropDownSlidIn.set(false);
+    this.topbarService.isDropDownDisabled = true;
+  }
 
 }
