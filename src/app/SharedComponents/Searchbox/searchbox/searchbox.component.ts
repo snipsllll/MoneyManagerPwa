0376d94681722789f894307020ed36238c4ac964
Tümmers, David <david.tuemmers@connext.de>
2024-11-21 09:26:35 +0100

    searchbox fixed

--- src/app/SharedComponents/Searchbox/searchbox/searchbox.component.ts
+++ src/app/SharedComponents/Searchbox/searchbox/searchbox.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searchbox',
@@ -9,7 +9,7 @@ export class SearchboxComponent {
   // Das zu filternde Array wird von außen als Input übergeben
   @Input() items: any[] = [];
 
-  // Der Name des Feldes, nach dem gefiltert wird (z.B. "titel")
+  // Der Name des Feldes, nach dem gefiltert wird (z.B. "data.title")
   @Input() filterField: string = '';
 
   // EventEmitter, um das geklickte Element nach außen zu senden
@@ -20,9 +20,15 @@ export class SearchboxComponent {
 
   // Methode zum Filtern der Items nach dem gegebenen Feld
   get gefilterteItems() {
-    return this.items.filter(item =>
-      item[this.filterField]?.toLowerCase().includes(this.suchbegriff.toLowerCase())
-    );
+    return this.items.filter(item => {
+      const value = this.getNestedFieldValue(item, this.filterField);
+      return value?.toLowerCase().includes(this.suchbegriff.toLowerCase());
+    });
+  }
+
+  // Hilfsmethode zum Zugriff auf verschachtelte Felder
+  getNestedFieldValue(item: any, fieldPath: string): any {
+    return fieldPath.split('.').reduce((obj, key) => obj?.[key], item);
   }
 
   // Methode, um das geklickte Element auszugeben
