82a2539c83f050775c2d0c7d2e05279fa948f163
Tümmers, David <david.tuemmers@connext.de>
2024-12-16 11:55:13 +0100

    register client

--- src/app/Sites/register/register.component.ts
+++ src/app/Sites/register/register.component.ts
@@ -8,6 +8,12 @@ import {Router} from "@angular/router";
 })
 export class RegisterComponent {
 
+  email?: string;
+  pw1?: string;
+  pw2?: string;
+
+  errorMessage?: string = '';
+
   constructor(private router: Router) {
   }
 
@@ -16,6 +22,36 @@ export class RegisterComponent {
   }
 
   onRegisterClicked() {
+    if(!this.email) {
+      this.errorMessage = 'Bitte geben Sie eine Email-adresse an!'
+      return;
+    }
+
+    if(!this.pw1) {
+      this.errorMessage = 'Bitte geben Sie ein Passwort an!'
+      return;
+    }
+
+    if(this.pw1.length < 6) {
+      this.errorMessage = 'Das Passwort muss mindestens 6 Zeichen enthalten!'
+      return;
+    }
+
+    if(!this.pw2) {
+      this.errorMessage = 'Bitte geben Sie das Passwort zwei mal an!'
+      return;
+    }
+
+    if(this.pw1 != this.pw2) {
+      this.errorMessage = 'Die angegebene Passwörter stimmen nicht überein!'
+      return;
+    }
+
+    this.errorMessage = undefined;
+    this.register();
+  }
+
+  private register() {
 
   }
 
